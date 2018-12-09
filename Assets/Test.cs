d004aa995e633dfa75f55b42420ca271bed05903
nekotanguitar <nekokick0402@gmail.com>
2018-12-10 01:15:12 +0900

    lesson4 課題+発展課題

---
+++ Assets/Test.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+    private int mp = 53;
+    private int cost = 5;
+
+    public void Magic()
+    {
+        this.mp -= this.cost;
+        Debug.Log("魔法攻撃をした。残りmpは" + this.mp + "。");
+        if(this.mp <= this.cost)
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+}
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        Boss lastboss = new Boss();
+        for (int i = 0; i < 10; i++)
+        {
+        lastboss.Magic();
+        }
+    }
+}
---
+++ Assets/kadai.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kadai : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int[] array = { 7, 8, 3, 5, 9 };
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+        for (int i = array.Length - 1; 0 <= i; i--)
+        {
+            Debug.Log(array[i]);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
